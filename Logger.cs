238779185032914867d45ac0b1d1599a11cf8442
Stuart Carruthers <stu@ironiclensflare.com>
2019-09-08 17:23:06 +0100

    Added log4net

---
+++ Logger.cs
@@ -0,0 +1,33 @@
+using System.Reflection;
+using log4net;
+using log4net.Appender;
+using log4net.Layout;
+using log4net.Repository.Hierarchy;
+
+namespace logger
+{
+    public static class Logger
+    {
+        public static ILog GetLogger()
+        {
+            var logger = LogManager.GetLogger(Assembly.GetCallingAssembly().GetType());
+            var logRepository = LogManager.GetRepository(Assembly.GetEntryAssembly());
+            
+            var hierarchy = (Hierarchy)logRepository;
+            hierarchy.Root.AddAppender(GetConsoleAppender());
+            hierarchy.Configured = true;
+            
+            return logger;
+        }
+
+        private static IAppender GetConsoleAppender()
+        {
+            var consoleAppender = new ConsoleAppender();
+            var layout = new PatternLayout { ConversionPattern = "%date %-5level %logger - %message%newline" };
+            layout.ActivateOptions();
+            consoleAppender.Layout = layout;
+
+            return consoleAppender;
+        }
+    }
+}
